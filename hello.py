d827717d5838b3afdf757412a34d8f181387b57e
Mitchell Roger <roger.mitch27@gmail.com>
2022-07-26 10:16:22 +1000

    finished

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return '<h1 style= "text-align: center;color: red" >"Hello", World!"</h1>'\
+           '<img src="https://media.giphy.com/media/l0HlPZgHWCuJiPtSg/giphy.gif" >'
+
+
+@app.route("/<name>")
+def bye(name):
+    return f"bye hi {name}"
+
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug =True)
